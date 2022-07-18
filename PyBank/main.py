5a023a41da60d26d9a3145a4d4f5f9cbdd4b6627
John <107654605+JohnTorgerson@users.noreply.github.com>
2022-07-18 00:16:47 -0500

    Added files to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,41 +10,61 @@ with open(csvpath, encoding='utf') as csv_file:
 
 	print(csv_reader)
 
-	csv_header = next(csv_reader)
-    print(f"CSV Header: {csv_header}")
+	#csv_header = next(csv_reader)
+    #print(f"CSV Header: {csv_header}")
 	
-	# Define start values of loop
-	month = 0
-	change = 0
+	## Creat Column [2] for Change Calculation
 
+	# Define start values of loop
+	#month = 0
+	#change = 0
+	#i = csv_reader[1]
 	# Read each row and count them
-	#for row[1] in csv_reader:
+	#for row[0] in csv_reader:
 		#month = month + 1
 		#tMonths = month -1
 	#print(f"Months: {tMonths}")
 
-
+	#for row[2] in csv_reader
+		#(i, [change]) = (i +1) - i
 		
-	# The net total amount of "Profit/Losses" over the entire period
+	# The total amount of "Profit/Losses" over the entire period
+	#gross_profit = [change].sum()
 
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-	# for row in csv_reader:
-		#Average = int(row[Profit/Losses])
-	# print(Average)
+	# Mean of the change list
+	#AVG_progit = [change].mean()
 
-# The greatest increase in profits (date and amount) over the entire period
-# (find “max?” “find()”biggest winner+ output result and date/2 cells)
-# “gInc = max(2)”
 
-# The “min?” greatest decrease in profits (date and amount) over the entire period
-# (find biggest loser+ "same as previous")
-# “gDec = min(2)”
+	# The greatest increase in profits (date and amount) over the entire period
+	# (find “max?” “find()”biggest winner+ output result and date/2 cells)
+	# “gInc = max(2)”
+
+	# The “min?” greatest decrease in profits (date and amount) over the entire period
+	# (find biggest loser+ "same as previous")
+	# “gDec = min(2)”
+
 print(" ")
 print("Financial Analysis")
 print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 print(" ")
-#print(f"Total Months: {tMonths}")
-print(f"Change: " +"string")
+print(f"Total Months:" +"tMonths")
+print(f"Change: " +"gross_profit")
 print(f"Average Change: " +"$" +"Average")
 print(f"Greatest Increase in Profits: " + "Date + gInc")
 print(f"Greatest Decrease in Profits: " + "Date + gDec")
+
+# Set variable for output file
+output_file = os.path.join("PyBank")
+
+#  Open the output file
+with open(output_file, "w") as txtfile:
+	writer = csv.writer(txtfile, delimiter=' ', quoting=csv.QUOTE_NONE)
+	writer.writerow(".")
+	writer.writerow("Financial.Analysis")
+	writer.writerow("_._._._._._._._._._._._._._._._._")
+	writer.writerow(".")
+	writer.writerow(f"Total.Months:"+"tMonths")
+	writer.writerow(f"Change:."+"gross_profit")
+	writer.writerow(f"Average.Change:."+"$"+"Average")
+	writer.writerow(f"Greatest.Increase.in.Profits:."+"Date.+.gInc")
+	writer.writerow(f"Greatest.Decrease.in.Profits:."+"Date.+.gDec")
