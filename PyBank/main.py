9d33fb87d13e29c33d5a67e2df30df454c70e321
John <107654605+JohnTorgerson@users.noreply.github.com>
2022-07-17 21:26:18 -0500

    Finished Poll, appendingBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,35 +5,41 @@ import csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #Read the CSV File
-with open(csvpath, encoding='utf') as csv_file:
+with open(csvpath, encoding='utf') as csv_file:	
 	csv_reader = csv.reader(csv_file, delimiter=",")
+
+output_path = os.path.join('Resources', 'budget_data.csv')	
+with open(output_path, 'w') as csvfile:
+## Append Header Row
+	for row in csv_reader:
+		row.append(row[0])
+
 
 	#Read the header row first
 	csv_header = next(csv_file)
 	print(f"Header: {csv_header}")
+
 	
 	# Define start values of loop
-	month = 1
-	#prof = (row[2])
+	month = 0
+	change = 0
 
 	# Read each row and count them
 	for row in csv_reader:
- 		
+
 		month = month + 1
 	tMonths = month -1
 
-# result = int(row[Profits/Losses])
-	pLoss = sum(row[2])
-	print(pLoss)
+	print(f"Months: {month}")
+
+
 		
-	
 	# The net total amount of "Profit/Losses" over the entire period
-# (cumulative/sum of column)
-	# (row[2]) 
 
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# (average of column)
-# Average =
+	# for row in csv_reader:
+		#Average = int(row[Profit/Losses])
+	# print(Average)
 
 # The greatest increase in profits (date and amount) over the entire period
 # (find “max?” “find()”biggest winner+ output result and date/2 cells)
@@ -46,8 +52,8 @@ print(" ")
 print("Financial Analysis")
 print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 print(" ")
-print(f"Total Months: {tMonths}")
-print("Total: " + "  ")
-print("Average Change: " +"$" +"Average")
-print("Greatest Increase in Profits: " + "Date + gInc")
-print("Greatest Decrease in Profits: " + "Date + gDec")
+print(f"Total Months: Months")
+print(f"Change: " +"string")
+print(f"Average Change: " +"$" +"Average")
+print(f"Greatest Increase in Profits: " + "Date + gInc")
+print(f"Greatest Decrease in Profits: " + "Date + gDec")
